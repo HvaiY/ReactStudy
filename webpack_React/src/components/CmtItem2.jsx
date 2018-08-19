36c1f207befc14eea48769b975bc82708519659d
HvaiY <337646685@qq.com>
2018-08-19 11:48:01 +0800

    评论中完整使用 模块化 css

--- webpack_React/src/components/CmtItem2.jsx
+++ webpack_React/src/components/CmtItem2.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import cssobj from '@/css/cmtlist.css'
+import cssobj from '@/css/cmtitem.css'
 //使用封装的样式
 export default function CmtItem(props){
-    return <div >
-    <h1 id={cssobj.cmtTitle}>评论人：{props.user}</h1>
-    <p id={cssobj.cmtItme}>评论内容：{props.content}</p>
+    return <div className={cssobj.combox}>
+    <h1 className={cssobj.title}>评论人：{props.user}</h1>
+    <p className={cssobj.content}>评论内容：{props.content}</p>
 </div>
 }
